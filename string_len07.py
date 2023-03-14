fc55e4a3c1a1ebbdf4cf55ce661f273ae61a56a8
husan999 <husanshunqorov07@gmail.com>
2023-03-14 10:46:08 +0500

    good

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,20 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    if len(s1)%2==0 and len(s2)%2==0 and len (s3)%2==0:
+        return "[]"
+    elif len(s1)%2==1 and len(s2)%2==1 and  len(s3)%2==1:
+        return f"[{s1} , {s2} ,{s3}]"
+    elif len(s1)%2==1 and len(s2)%2==1:
+        return f"[{s1} ,{s2}]"
+    elif len(s1)%2==1 and len(s3)%2==1:
+        return f"[{s1} ,{s3}]"
+    elif len(s2)%2==1 and len(s3)%2==1:
+        return f"[{s2} ,{s3}]"
+    elif len(s1)%2==1:
+        return f"[{s1}]"
+    elif len(s2)%2==1:
+        return f"[{s2}]"
+    elif len(s3)%2==1:
+        return f"[{s3}]"
+    print(main("code","python","coder"))
